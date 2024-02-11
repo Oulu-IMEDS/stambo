56f6492f4d7c034b5d107003336d429c5dea3f93
lext <aleksei.tiulpin@protonmail.ch>
2024-02-11 19:45:55 +0200

    version fix

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/oulu-imeds/stambo",
-    version="0.0.1",
+    version="0.1.1",
     zip_safe=False,
 )
